987eb7b41bd68912bbb0c916b67c61283ccc94be
unknown <zhijianlow@gmail.com>
2024-09-15 15:48:20 +0800

    Week 5 progress, not sure how to make scoring and computer work

--- Go.js
+++ Go.js
@@ -1,11 +1,7 @@
 const COLORS = ["transparent", "black", "white"];
 let board = [];
-
-const make_board = (size) => {
-    board = [];
-    board = Array.from({ length: size }, () =>
-        new Array(size).fill(0));
-}
+let boardStates = new Set();
+let selfCapture = true;
 
 const sizeMap = new Map([
     [5, 580],
@@ -14,6 +10,10 @@ const sizeMap = new Map([
     [19, 665]
 ])
 let turn = 0;
+
+let boardSize = 0;
+
+//===================================UI METHODS===========================================//
 
 const hover = (turn, id) => {
     const stone = document.getElementById("s" + id);
@@ -31,50 +31,108 @@ const hoverLeave = (id) => {
     }
 }
 
-const make_stone = (x, y, id, size) => {
-    const edge = sizeMap.get(size);
-    const spacing = edge / size;
+const passTurn = () => {
+    turn += 1;
+}
+//===================================HELPER FUNCTIONS======================================//
 
-    const div = document.createElement('div');
-    const board = document.getElementById("boardLines");
-    board.appendChild(div);
-    div.className += "stone";
-    div.id = "s" + id;
-    div.style.position = "absolute";
-    div.style.height = spacing + "px";
-    div.style.width = spacing + "px";
-    div.style.left = (x * spacing - (spacing / 2)) + "px";
-    div.style.top = (y * spacing - (spacing / 2)) + "px";
-    div.onmouseenter = () => {
-        hover(turn, id);
+const boardToString = (board) => {
+    let boardString = "";
+    for (const column of board) {
+        for (const position of column) {
+            boardString += position.toString();
+        }
     }
-    div.onmouseleave = () => {
-        hoverLeave(id);
-    }
-    div.onclick = () => update_board(id);
-};
-
-const update_board = (id) => {
-    console.log(id);
-    if (board[id[0]][id[1]] == 0) {
-        board[id[0]][id[1]] = (turn % 2) + 1;
-        turn++;
-    }
-    update_draw(id); 
-};
-
-const update_draw = (id) => {
-    const stone = document.getElementById("s" + id);
-    stone.style.opacity = 1;
-    stone.style.background = COLORS[board[id[0]][id[1]]];
+    return boardString;
 }
 
-const layout_stones = (size) => {
-    for (let id = 0; id < size * size; id++) {
-        make_stone(id % size, Math.floor(id / size), [id % size, Math.floor(id / size)], size);
-    }
-};
+const idToString = (id) => {
+    return (id[0] + "," + id[1]);
+}
 
+const fetchNeighbors = (id, color) => {
+    let neighbors = [];
+    let foundEmpty = false;
+    if (id[0] > 0) {
+        const leftID = [id[0] - 1, id[1]];
+        const leftState = board[leftID[0]][leftID[1]];
+        if (leftState == color) {
+            neighbors.push(leftID);
+        }
+        else if (leftState == 0) {
+            foundEmpty = true;
+        }
+    }
+    if (id[1] > 0) {
+        const upID = [id[0], id[1] - 1];
+        const upState = board[upID[0]][upID[1]];
+        if (upState == color) {
+            neighbors.push(upID);
+        }
+        else if (upState == 0) {
+            foundEmpty = true;
+        }
+    }
+    if (id[0] < boardSize - 1) {
+        const rightID = [id[0] + 1, id[1]];
+        const rightState = board[rightID[0]][rightID[1]];
+        if (rightState == color) {
+            neighbors.push(rightID);
+        }
+        else if (rightState == 0) {
+            foundEmpty = true;
+        }
+    }
+    if (id[1] < boardSize - 1) {
+        const downID = [id[0], id[1] + 1];
+        const downState = board[downID[0]][downID[1]];
+        if (downState == color) {
+            neighbors.push(downID);
+        }
+        else if (downState == 0) {
+            foundEmpty = true;
+        }
+    }
+    return {
+        foundEmpty: foundEmpty,
+        neighbors: neighbors
+    }
+}
+
+//try BFS first as long chain not as likely?
+const selfCapturedCheck = (id, color) => {
+    let queue = [id];
+    let pointer = 0;
+    let explored = new Set();
+
+    explored.add(idToString(id));
+    console.log(color);
+    while ((pointer < queue.length)) {
+        const neighborMap = fetchNeighbors(queue[pointer], color);
+        const foundEmpty = neighborMap.foundEmpty;
+        const neighbors = neighborMap.neighbors;
+        console.log(neighbors, color)
+        if (foundEmpty) {
+            return [];
+        }
+        else {
+            for (const neighbor of neighbors) {
+                //console.log(neighbor);
+                if (!explored.has(idToString(neighbor))) {
+                    console.log("explored", explored, "pushing", neighbor);
+                    queue.push(neighbor);
+                    explored.add(idToString(neighbor));
+                }
+            }
+        }
+        pointer = pointer + 1;
+        console.log("im here", queue, pointer);
+    }
+    //if surrounded, return chain to be captured, else return empty array.
+    return queue;
+}
+
+//===================================SET UP===========================================//
 const go_board = (size) => {
     const edge = sizeMap.get(size);
     const spacing = edge / size;
@@ -100,31 +158,136 @@ const go_board = (size) => {
     ctx.stroke();
 };
 
+const layout_stones = (size) => {
+    for (let id = 0; id < size * size; id++) {
+        make_stone(id % size, Math.floor(id / size), [id % size, Math.floor(id / size)], size);
+    }
+};
+
+const make_board = (size) => {
+    turn = 0;
+    boardStates = new Set();
+    board = [];
+    board = Array.from({ length: size }, () =>
+        new Array(size).fill(0));
+}
+
+const make_stone = (x, y, id, size) => {
+    const edge = sizeMap.get(size);
+    const spacing = edge / size;
+
+    const div = document.createElement('div');
+    const board = document.getElementById("boardLines");
+    board.appendChild(div);
+    div.className += "stone";
+    div.id = "s" + id;
+    div.style.position = "absolute";
+    div.style.height = spacing + "px";
+    div.style.width = spacing + "px";
+    div.style.left = (x * spacing - (spacing / 2)) + "px";
+    div.style.top = (y * spacing - (spacing / 2)) + "px";
+    div.onmouseenter = () => {
+        hover(turn, id);
+    }
+    div.onmouseleave = () => {
+        hoverLeave(id);
+    }
+    div.onclick = () => update_board(id);
+};
+
+//===================================STUFFS===========================================//
+
+const update_board = (id) => {
+    let captured = []
+    if (board[id[0]][id[1]] == 0) {
+        const previousBoard = board.map(inner => inner.slice());
+        board[id[0]][id[1]] = (turn % 2) + 1;
+        //check for captures
+        const neighborMap = fetchNeighbors(id, ((turn + 1) % 2) + 1);
+        const captureCheck = neighborMap.neighbors; 
+        console.log(captureCheck);
+        for (const neighbor of captureCheck) {
+            captured = captured.concat(selfCapturedCheck(neighbor, ((turn + 1) % 2) + 1));
+        }
+        if (captured.length == 0 && selfCapture) {
+            //check for self capture
+            captured = selfCapturedCheck(id, (turn % 2) + 1);
+        }
+        //remove all stones from captured
+        if (captured.length > 0) {
+            for (const capture of captured) {
+                board[capture[0]][capture[1]] = 0;
+            }
+        }
+        //check if final board state has been reached before and return to previous if yes.
+        if (boardStates.has(boardToString(board))) {
+            board = previousBoard;
+            captured = [];
+        }
+        else {
+            boardStates.add(boardToString(board));
+            turn++;
+        }
+    }
+    update_draw(id); 
+    for (const capture of captured) {
+        update_draw(capture);
+    }
+};
+
+const update_draw = (id) => {
+    const stone = document.getElementById("s" + id);
+    stone.style.opacity = 1;
+    stone.style.background = COLORS[board[id[0]][id[1]]];
+}
 
 
-//go_board(19);
+//===================================ONCLICK EVENTS===========================================//
 document.getElementById("5x5").onclick = () => {
     go_board(5);
     layout_stones(5);
     make_board(5);
+    boardSize = 5;
 };
 
 document.getElementById("9x9").onclick = () => {
     go_board(9);
     layout_stones(9);
     make_board(9);
-
+    boardSize = 9;
 };
 
 document.getElementById("13x13").onclick = () => {
     go_board(13);
     layout_stones(13);
     make_board(13);
-
+    boardSize = 13;
 };
 
 document.getElementById("19x19").onclick = () => {
     go_board(19);
     layout_stones(19);
     make_board(19);
+    boardSize = 19;
+};
+
+document.getElementById("Pass").onclick = () => {
+    passTurn();
+};
+
+document.getElementById("Self Capture").onclick = () => {
+    selfCapture = !selfCapture;
+    const current = document.getElementById('Self Capture');
+    if (selfCapture) {
+        current.innerHTML = "Self Capture On";
+    }
+    else {
+        current.innerHTML = "Self Capture Off";
+    }
+};
+
+document.onkeydown = function (e) {
+    if (e.code === "Space") {
+        passTurn();
+    }
 };
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Go
 Go Project for ME4101A
+deployed at https://pleonexia-jian.github.io/Go/
+
+Further tasks:
+Scoring
+scoring is a little too hard? assuming that we are using chinese Go rules(weiqi), the scoring system would still need to be
+able to identify territories and seki. in Seki(neutral territory fully surrounded by either players), players need to fill in 
+the empty points with their color to get more points. They would fill in false eyes in the seki as well to get more points.
+Therefore, the computer would need to be able to identify false eyes as well. However, the computer cannot fill in real eyes 
+or they will put themselves in self-capture or being up for capture.
+
+For neutral territory, the computer would either fill in the teritory and count the stones or I would have a algo to identify
+if fully surrounded by a solid color. If the computer fill in the teritory, they need to make eyes and not fill it all in
+to perform self-capture or put themselves at risk for capture. If i disable self-capturing and allow placing stones during scoring?
+i dont even know how im supposed to identify seki in the first place.
+
+![Image of a Seki](image.png)
+![correct resolution of that seki](image-1.png)
+
+Computer
